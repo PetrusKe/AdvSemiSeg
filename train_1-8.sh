b118cef038332e44de742df1044ec5eb087aa41e
ZHKKKe <kezhanghan@outlook.com>
2019-10-09 23:08:59 +0800

    update script

--- train_1-8.sh
+++ train_1-8.sh
@@ -1,6 +1,6 @@
 python train.py --snapshot-dir snapshots_1-8 \
                 --partial-data 0.125 \
-                --num-steps 20000 \
+                --num-steps 40000 \
                 --lambda-adv-pred 0.01 \
                 --save-dir results_1-8 \
                 --lambda-semi 0 --semi-start 5000 --mask-T 0.2
